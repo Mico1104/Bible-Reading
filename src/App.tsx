a0d666331fa091f10c7b9f676efeee4d86a50f1b
Mico1104 <michealmarvellous782@gmail.com>
2026-08-21 18:57:41 +0100

    Setting up queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from "react-router-dom";
 import { useAuthStore } from "./stores/authStore";
 import { router } from "./router";
 import { useEffect } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 function App() {
   const initialize = useAuthStore((state) => state.initialize);
@@ -12,9 +14,11 @@ function App() {
   }, [initialize]);
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12">
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen bg-gray-50 py-12">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
