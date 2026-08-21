959f0ce2d308516e2daa5e13f1b90d73182d9c5c
Mico1104 <michealmarvellous782@gmail.com>
2026-08-21 19:14:18 +0100

    useProfile added  by using React Query to fetch user's name from the supabase and display it on the dashboard

---
+++ src/features/auth/useProfile.ts
@@ -0,0 +1,23 @@
+import { supabase } from "@/lib/supabase";
+import { useAuthStore } from "@/stores/authStore";
+import { useQuery } from "@tanstack/react-query";
+
+export const useProfile = () => {
+  const userId = useAuthStore((state) => state.user?.id);
+
+  return useQuery({
+    queryKey: ["profile", userId],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("name, username")
+        .eq("id", userId)
+        .single();
+
+      if (error) throw error;
+      return data;
+    },
+
+    enabled: !!userId,
+  });
+};
--- src/features/daily-reading/DashboardPage.tsx
+++ src/features/daily-reading/DashboardPage.tsx
@@ -1,3 +1,16 @@
+import { useProfile } from "../auth/useProfile";
+
 export const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const { data: profile, isLoading } = useProfile();
+
+  if (isLoading) {
+    return <p className="p-6 text-gray-500">Loading...</p>;
+  }
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">
+        Welcome, {profile?.name ?? profile?.username}
+      </h1>
+    </div>
+  );
 };
