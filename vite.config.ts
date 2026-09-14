804fa6df74124cea986657cad51dee66d1fe9cad
Mico1104 <michealmarvellous782@gmail.com>
2026-09-14 22:04:49 +0100

    add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,31 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Daily Word",
+        short_name: "Daily Word",
+        description:
+          "Build a daily Bible reading habit, track your progress, and grow at your own pace.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+        icons: [
+  {
+    src: "/book.svg",
+    sizes: "800x800",
+    type: "image/svg+xml",
+  },
+],
+      },
+    }),
   ],
+
   resolve: {
     alias: {
       "@": "/src",
